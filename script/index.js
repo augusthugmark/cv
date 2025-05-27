3cce9ceddf2e4223976f40fe02dff61be97c704d
August Hugmark <august.hugmark@gmail.com>
2025-05-27 18:54:40 +0200

    refactor project structure

---
+++ script/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const reveals = document.querySelectorAll('.reveal');
+
+const revealObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+reveals.forEach(el => revealObserver.observe(el));
